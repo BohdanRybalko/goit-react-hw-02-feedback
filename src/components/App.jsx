c30d444b95439ef5070766225a0e46a2f11b6d87
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-08 20:25:58 +0200

    create feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
+import React, { useState } from 'react';
+import Statistics from 'components/Statistics';
+import FeedbackOptions from 'components/FeedbackOptions';
+import Section from 'components/Section';
+import Notification from 'components/Notification';
+
 export const App = () => {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const countTotalFeedback = () => {
+    return state.good + state.neutral + state.bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total === 0 ? 0 : Math.round((state.good / total) * 100);
+  };
+
+  const onLeaveFeedback = (option) => {
+    setState((prevState) => ({ ...prevState, [option]: prevState[option] + 1 }));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title="Leave Feedback">
+        <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={state.good}
+            neutral={state.neutral}
+            bad={state.bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        )}
+      </Section>
     </div>
   );
 };
+
+export default App;
---
+++ src/components/Notification.jsx
@@ -0,0 +1,8 @@
+// Notification.jsx
+import React from 'react';
+
+const Notification = ({ message }) => {
+  return <p>{message}</p>;
+};
+
+export default Notification;
