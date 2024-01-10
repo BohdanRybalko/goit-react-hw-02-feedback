5ee2446eb9421dd187122d5bd3a2df36e99b62c6
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-11 00:41:10 +0200

    fix sextion and notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Notification extends Component {
-  render() {
-    const { message } = this.props;
-    return <p>{message}</p>;
-  }
-}
+const Notification = ({ message }) => {
+  return <p>{message}</p>;
+};
 
 export default Notification;
