09f0d6d818ae0704406f16aa0fc900a602e0b58a
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-01-11 00:43:35 +0200

    fix feedbackoptions

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FeedbackOptions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    return (
-      <div>
-        {options.map((option) => (
-          <button key={option} onClick={() => onLeaveFeedback(option)}>
-            {option}
-          </button>
-        ))}
-      </div>
-    );
-  }
-}
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map((option) => (
+        <button key={option} onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 export default FeedbackOptions;
